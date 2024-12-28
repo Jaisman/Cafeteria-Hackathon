2f89931ce1d6cf4fa629885de4c7d0047f5aaed0
Jaisman <149098952+Jaisman@users.noreply.github.com>
2024-12-28 11:05:47 +0530

    Update Otp_verify.jsx

--- Frontend/src/components/Otp_verify/Otp_verify.jsx
+++ Frontend/src/components/Otp_verify/Otp_verify.jsx
@@ -18,7 +18,7 @@ export default function OtpVerify({ order }) {
     e.preventDefault();
     
       try{
-      const res = await axios.post(`https://cafeteria-hackathon-gqvh.vercel.app/api/verify/${_id}/verifyotp`,{verifyotp:inotp});
+      const res = await axios.post(`https://cafeteria-hackathon.vercel.app/api/verify/${_id}/verifyotp`,{verifyotp:inotp});
       setMessage("");
         navigate("/menu")
       }
